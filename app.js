8dbae78390136c190b782d170124f67deba0adca
Sm4ck <mikeamartinez314@gmail.com>
2022-06-30 17:56:39 -0500

    Create Project Section HTML

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import inquirer from 'inquirer';
+import fs from 'fs'
+import generatePage from './src/page-template.js';
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -127,11 +129,15 @@ if (!portfolioData.projects) {
       });
   };
 
-  promptUser()
+promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw err;
+        console.log('Portfolio complete! Check out index.html to see the output!');
+    });
+});
 
 
 
@@ -152,10 +158,4 @@ if (!portfolioData.projects) {
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
